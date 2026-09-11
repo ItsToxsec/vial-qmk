f2bcd37bc4a6bda5e087e380dc9abf060289a780
Daniel <itstoxsec@gmail.com>
2026-09-11 11:25:57 -0400

    Update keymap.c

--- keyboards/keyball/keyball61/keymaps/vial/keymap.c
+++ keyboards/keyball/keyball61/keymaps/vial/keymap.c
@@ -1,24 +1,4 @@
-/*
-Copyright 2022 @Yowkees
-Copyright 2022 MURAOKA Taro (aka KoRoN, @kaoriya)
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include QMK_KEYBOARD_H
-#include "quantum.h"
-#include "sly_oled.h"
 
 enum layers {
     QWE = 0,
@@ -38,23 +18,6 @@ enum layers {
     MOUSE
 };
 
-#define HM_LALT_X    LALT_T(KC_X)
-#define HM_LCTL_C    LCTL_T(KC_C)
-#define HM_LSFT_V    LSFT_T(KC_V)
-
-#define HM_RSFT_M    RSFT_T(KC_M)
-#define HM_RCTL_COMM RCTL_T(KC_COMM)
-#define HM_RALT_DOT  RALT_T(KC_DOT)
-#define HM_LSFT_D    LSFT_T(KC_D)
-
-/*
- * Convenient aliases.
- */
-#define _______ KC_TRNS
-#define XXXXXXX KC_NO
-
-// clang-format off
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [QWE] = LAYOUT_right_ball(
@@ -62,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_EQL, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
         KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
         KC_GRV, KC_Z, LALT_T(KC_X), LCTL_T(KC_C), LSFT_T(KC_V), KC_B, KC_QUOT, KC_MINS, KC_N, RSFT_T(KC_M), RCTL_T(KC_COMMA), RALT_T(KC_DOT), KC_SLSH, KC_PGUP,
-        KC_UP, KC_DOWN, KC_LEFT, KC_RGHT, KC_LSFT, LT(13, KC_BSPC), LT(11, KC_DEL), LT(10, KC_ENT), LT(12, KC_SPC), KC_RGUI, KC_PGDN
+        KC_UP, KC_DOWN, KC_LEFT, KC_RGHT, KC_LSFT, LT(11, KC_BSPC), LT(13, KC_DEL), LT(12, KC_ENT), LT(10, KC_SPC), KC_RGUI, KC_PGDN
     ),
 
     [GAL] = LAYOUT_right_ball(
@@ -174,126 +137,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, MS_WHLL, MS_WHLU, MS_WHLD, MS_WHLR, KC_NO,
         KC_NO, KC_NO, MS_BTN3, MS_BTN2, MS_BTN1, SCRL_TO, CPI_D100, CPI_I100, KC_NO, MS_BTN1, MS_BTN2, MS_BTN3, KC_NO, KC_NO,
-        UG_TOGG, KC_NO, KC_NO, KC_NO, KC_NO, KC_LALT, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO
+        RGB_TOG, KC_NO, KC_NO, KC_NO, KC_NO, KC_LALT, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO
     )
 };
-
-// clang-format on
-
-
-/*
- * Per-layer RGB colors.
- * RGBLIGHT applies the selected color to all LEDs on both halves.
- */
-static void set_layer_color(uint8_t layer) {
-#ifdef RGBLIGHT_ENABLE
-    switch (layer) {
-        case QWE:   rgblight_setrgb(0x05, 0x8C, 0xFA); break; // #058cfa
-        case GAL:   rgblight_setrgb(0x05, 0x8C, 0xFA); break; // #058cfa
-        case COL:   rgblight_setrgb(0x05, 0x8C, 0xFA); break; // #058cfa
-        case GAM:   rgblight_setrgb(0xF7, 0x50, 0x93); break; // #f75093
-        case NUM:   rgblight_setrgb(0x83, 0xA8, 0xC7); break; // #83a8c7
-        case FUN:   rgblight_setrgb(0x27, 0x04, 0xC2); break; // #2704c2
-        case NAV:   rgblight_setrgb(0xD6, 0x81, 0x0B); break; // #d6810b
-        case MED:   rgblight_setrgb(0xC2, 0x04, 0x69); break; // #c20469
-        case L08:   rgblight_setrgb(0x05, 0x8C, 0xFA); break;
-        case L09:   rgblight_setrgb(0x05, 0x8C, 0xFA); break;
-        case L10:   rgblight_setrgb(0x05, 0x8C, 0xFA); break;
-        case L11:   rgblight_setrgb(0x05, 0x8C, 0xFA); break;
-        case L12:   rgblight_setrgb(0x05, 0x8C, 0xFA); break;
-        case L13:   rgblight_setrgb(0x05, 0x8C, 0xFA); break;
-        case MOUSE: rgblight_setrgb(0x40, 0x40, 0x40); break; // dim white for MOUSE // #ffffff
-        default:    rgblight_setrgb(0x05, 0x8C, 0xFA); break;
-    }
-#endif
-}
-
-void keyboard_post_init_user(void) {
-#ifdef RGBLIGHT_ENABLE
-    rgblight_enable_noeeprom();
-    set_layer_color(get_highest_layer(layer_state));
-#endif
-}
-
-/*
- * Latched mouse layer behavior:
- * - Trackball movement turns MOUSE on from keyball.c.
- * - MOUSE stays on until a non-mouse key is pressed.
- * - Mouse keys do not dismiss the layer.
- */
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (record->event.pressed && layer_state_is(MOUSE)) {
-        switch (keycode) {
-            case MS_BTN1:
-            case MS_BTN2:
-            case MS_BTN3:
-            case MS_BTN4:
-            case MS_BTN5:
-            case MS_LEFT:
-            case MS_RGHT:
-            case MS_UP:
-            case MS_DOWN:
-            case MS_WHLL:
-            case MS_WHLR:
-            case MS_WHLU:
-            case MS_WHLD:
-                break;
-            default:
-                layer_off(MOUSE);
-                break;
-        }
-    }
-    return true;
-}
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-    set_layer_color(get_highest_layer(state));
-    return state;
-}
-
-
-/*
- * Do NOT keep the original:
- *
- *   keyball_set_scroll_mode(get_highest_layer(state) == 3);
- *
- * because layer 3 is now the GAMING layer.
- *
- * With this layout the trackball remains in normal pointer mode.
- *
- * If you later want NAV to automatically turn the trackball into
- * a scroll wheel, replace this comment with:
- *
- * layer_state_t layer_state_set_user(layer_state_t state) {
- *     keyball_set_scroll_mode(get_highest_layer(state) == NAV);
- *     return state;
- * }
- */
-
-
-#ifdef OLED_ENABLE
-
-#    include "lib/oledkit/oledkit.h"
-#    include "sly_oled.h"
-
-/*
- * Master side:
- *   Keyball status information
- */
-void oledkit_render_info_user(void) {
-    keyball_oled_render_keyinfo();
-    keyball_oled_render_ballinfo();
-    keyball_oled_render_layerinfo();
-}
-
-/*
- * Secondary side:
- *   custom logo
- */
-void oledkit_render_logo_user(void) {
-    oled_clear();
-    oled_set_cursor(0, 0);
-    render_sly_logo();
-}
-
-#endif
